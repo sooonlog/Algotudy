725f8e3c3261c2a6c0d4eecad8638272ebf74289
sooonlog <gytns0100@gmail.com>
2022-06-15 09:05:01 +0900

    JS100제 8.객체의 키 이름 중복

---
+++ 220615.js
@@ -0,0 +1,13 @@
+// 아래 코드의 출력값을 입력하시오. (출력값은 공백을 넣지 않습니다. )
+
+// var d = {
+//   'height':180,
+//   'weight':78,
+//   'weight':84,
+//   'temperature':36,
+//   'eyesight':1
+// };
+
+// console.log(d['weight']);
+
+// 정답: 84
